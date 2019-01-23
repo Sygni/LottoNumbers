840d22d6dbe6ab3f0eed5ce6b770b53606738f60
sygni <sygni.grit@gmail.com>
2019-01-24 02:19:25 +0900

    table view

---
+++ LottoNumbers/LottoNumbers/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  LottoNumbers
+//
+//  Created by Identité on 23/01/2019.
+//  Copyright © 2019 Identité. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //Dispose of any resources that can be recreated
+    }
+  
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+  
+    
+}
+
