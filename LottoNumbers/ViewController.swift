67dc720daee4fd9aa42e21dad5311431ff85e969
sygni <sygni.grit@gmail.com>
2019-01-23 20:40:38 +0900

    first commit

--- LottoNumbers/ViewController.swift
+++ LottoNumbers/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //Dispose of any resources that can be recreated
+    }
+    
+    @IBAction func touchAction(_ sender: UIButton) {
+        //change text in label
+        label.text = "Hello Ayang!"
+    }
+    
 }
 
